066a7f69bcaca22320d6f2274221aef6992a1e12
dmtrack <rightrack888@gmail.com>
2023-11-28 19:39:14 +0500

    init: client

---
+++ client/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <main className='App'>
+            <section className='main_container'>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className='App-link'
+                    href='https://reactjs.org'
+                    target='_blank'
+                    rel='noopener noreferrer'>
+                    Learn React
+                </a>
+            </section>
+        </main>
+    );
+};
+
+export default App;
