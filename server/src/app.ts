2fc8c5034359fe31079db4f3fc67e9c361cade5d
dmtrack <rightrack888@gmail.com>
2023-11-28 18:56:23 +0500

    feat: validation is added

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { urlencoded, json } from 'body-parser';
+import { Request, Response, NextFunction } from 'express';
+const util = require('util');
+import { body, validationResult } from 'express-validator';
 import cors from 'cors';
 import http from 'http';
 import { mokeData } from './moke';
@@ -17,21 +20,30 @@ app.use(cors({ origin: process.env.CLIENT_URL, optionsSuccessStatus: 200 }));
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
-app.post('/getuser', async (req, res) => {
-    const { email, number } = req.body;
+app.post(
+    '/user',
+    body('email').isEmail(),
+    body('email').isLength({ min: 5 }),
 
-    setTimeout(() => {
-        const filteredData: IUser[] = mokeData.filter((rec) => {
-            if (email && number) {
-                return rec.email === email && rec.number === String(number);
-            } else if (email && !number) {
-                return rec.email === email;
-            }
-        });
-        if (filteredData.length > 0) {
-            res.status(200).json(filteredData);
-        } else {
-            res.status(400).send('there is no such user');
+    (req: Request, res: Response) => {
+        const { email, number } = req.body;
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
         }
-    }, 2000);
-});
+        setTimeout(() => {
+            const filteredData: IUser[] = mokeData.filter((rec) => {
+                if (email && number) {
+                    return rec.email === email && rec.number === String(number);
+                } else if (email && !number) {
+                    return rec.email === email;
+                }
+            });
+            if (filteredData.length > 0) {
+                res.status(200).json(filteredData);
+            } else {
+                res.status(400).send('there is no such user');
+            }
+        }, 5000);
+    }
+);
