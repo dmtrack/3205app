1159af8a0abb124c8fecf83fd379fa44e87528ff
dmtrack <rightrack888@gmail.com>
2023-11-29 18:55:54 +0500

    fix: error in filter

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { urlencoded, json } from 'body-parser';
-import { Request, Response, NextFunction } from 'express';
-const util = require('util');
+import { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import cors from 'cors';
 import http from 'http';
@@ -10,31 +9,31 @@ import { mokeData } from './moke';
 import { IUser } from './interfaces/user.interface';
 
 const url = process.env.CLIENT_URL || 'http://localhost';
+const port = process.env.CLIENT_PORT || 3000;
 
 export const app = express();
 export const server = http.createServer(app);
 dotenv.config();
-app.options('*', cors({ origin: url, credentials: true }));
 
-app.use(cors({ origin: process.env.CLIENT_URL, optionsSuccessStatus: 200 }));
+app.use('*', cors({ origin: `${url}:${port}`, credentials: true }));
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
 app.post(
     '/user',
     body('email').isEmail(),
-    body('email').isLength({ min: 5 }),
+    body('email').isLength({ min: 6 }),
 
     (req: Request, res: Response) => {
         const { email, number } = req.body;
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+            return res.status(400).send(errors);
         }
         setTimeout(() => {
             const filteredData: IUser[] = mokeData.filter((rec) => {
                 if (email && number) {
-                    return rec.email === email && rec.number === String(number);
+                    return rec.number === number.split('-').join('');
                 } else if (email && !number) {
                     return rec.email === email;
                 }
